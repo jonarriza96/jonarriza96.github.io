f7fcdebfc057a90c74093e7e1423fbcca2a5dab2
Jon Arrizabalaga Aguirregomezcorta <jon.arrizabalaga@tum.de>
2022-11-12 15:08:04 +0100

    new version

---
+++ w3/w3images/inx_CartDetailView.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<title>Detail View</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+ 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+	
+
+<!----------------------------------------------------------------------------------------->
+Connection failed: php_network_getaddresses: getaddrinfo failed: Name or service not known
